0ca66de6aeb88ec34c43c18471b47a554a69f528
filippennsater <filip.pennsater@hotmail.com>
2023-03-22 00:37:44 +0100

    fixed sound for every note and bound to keys

--- Views-Stylesheets-Scripts/script.js
+++ Views-Stylesheets-Scripts/script.js
@@ -1,15 +1,15 @@
-const A1min = new Audio("assets/piano-A-1.mp3");
-const A2min = new Audio("assets/piano-A-2.mp3");
-const A3min = new Audio("assets/piano-A-3.mp3");
-const A4min = new Audio("assets/piano-A-4.mp3");
-const A5min = new Audio("assets/piano-A-5.mp3");
-const A6min = new Audio("assets/piano-A-6.mp3");
-const A7min = new Audio("assets/piano-A-7.mp3");
-const A8min = new Audio("assets/piano-A-8.mp3");
-const A9min = new Audio("assets/piano-A-9.mp3");
-const A10min = new Audio("assets/piano-A-10.mp3");
-const A11min = new Audio("assets/piano-A-11.mp3");
-const A12min = new Audio("assets/piano-A-12.mp3");
+const A1min = new Audio("assets/piano-A-m1.mp3");
+const A2min = new Audio("assets/piano-A-m2.mp3");
+const A3min = new Audio("assets/piano-A-m3.mp3");
+const A4min = new Audio("assets/piano-A-m4.mp3");
+const A5min = new Audio("assets/piano-A-m5.mp3");
+const A6min = new Audio("assets/piano-A-m6.mp3");
+const A7min = new Audio("assets/piano-A-m7.mp3");
+const A8min = new Audio("assets/piano-A-m8.mp3");
+const A9min = new Audio("assets/piano-A-m9.mp3");
+const A10min = new Audio("assets/piano-A-m10.mp3");
+const A11min = new Audio("assets/piano-A-m11.mp3");
+const A12min = new Audio("assets/piano-A-m12.mp3");
 
 const A1 = new Audio("assets/piano-A-1.mp3");
 const A2 = new Audio("assets/piano-A-2.mp3");
@@ -24,18 +24,18 @@ const A10 = new Audio("assets/piano-A-10.mp3");
 const A11 = new Audio("assets/piano-A-11.mp3");
 const A12 = new Audio("assets/piano-A-12.mp3");
 
-const A1plus = new Audio("assets/piano-A-1.mp3");
-const A2plus = new Audio("assets/piano-A-2.mp3");
-const A3plus = new Audio("assets/piano-A-3.mp3");
-const A4plus = new Audio("assets/piano-A-4.mp3");
-const A5plus = new Audio("assets/piano-A-5.mp3");
-const A6plus = new Audio("assets/piano-A-6.mp3");
-const A7plus = new Audio("assets/piano-A-7.mp3");
-const A8plus = new Audio("assets/piano-A-8.mp3");
-const A9plus = new Audio("assets/piano-A-9.mp3");
-const A10plus = new Audio("assets/piano-A-10.mp3");
-const A11plus = new Audio("assets/piano-A-11.mp3");
-const A12plus = new Audio("assets/piano-A-12.mp3");
+const A1p = new Audio("assets/piano-A-p1.mp3");
+const A2p = new Audio("assets/piano-A-p2.mp3");
+const A3p = new Audio("assets/piano-A-p3.mp3");
+const A4p = new Audio("assets/piano-A-p4.mp3");
+const A5p = new Audio("assets/piano-A-p5.mp3");
+const A6p = new Audio("assets/piano-A-p6.mp3");
+const A7p = new Audio("assets/piano-A-p7.mp3");
+const A8p = new Audio("assets/piano-A-p8.mp3");
+const A9p = new Audio("assets/piano-A-p9.mp3");
+const A10p = new Audio("assets/piano-A-p10.mp3");
+const A11p = new Audio("assets/piano-A-p11.mp3");
+const A12p = new Audio("assets/piano-A-p12.mp3");
 
 function playSound(tone) {
 
@@ -45,48 +45,48 @@ function playSound(tone) {
             A1min.play();
             break;
         case -2:
-            A2.currentTime = 0;
-            A2.play();
+            A2min.currentTime = 0;
+            A2min.play();
             break;
         case -3:
-            A3.currentTime = 0;
-            A3.play();
+            A3min.currentTime = 0;
+            A3min.play();
             break;
         case -4:
-            A4.currentTime = 0;
-            A4.play();
+            A4min.currentTime = 0;
+            A4min.play();
             break;
         case -5:
-            A5.currentTime = 0;
-            A5.play();
+            A5min.currentTime = 0;
+            A5min.play();
             break;
         case -6:
-            A6.currentTime = 0;
-            A6.play();
+            A6min.currentTime = 0;
+            A6min.play();
             break;
         case -7:
-            A7.currentTime = 0;
-            A7.play();
+            A7min.currentTime = 0;
+            A7min.play();
             break;
         case -8:
-            A8.currentTime = 0;
-            A8.play();
+            A8min.currentTime = 0;
+            A8min.play();
             break;
         case -9:
-            A9.currentTime = 0;
-            A9.play();
+            A9min.currentTime = 0;
+            A9min.play();
             break;
         case -10:
-            A10.currentTime = 0;
-            A10.play();
+            A10min.currentTime = 0;
+            A10min.play();
             break;
         case -11:
-            A11.currentTime = 0;
-            A11.play();
+            A11min.currentTime = 0;
+            A11min.play();
             break;
         case -12:
-            A12.currentTime = 0;
-            A12.play();
+            A12min.currentTime = 0;
+            A12min.play();
             break;
 
 
@@ -140,52 +140,52 @@ function playSound(tone) {
             break;
 
             case 100:
-                A1.currentTime = 0;
-                A1.play();
+                A1p.currentTime = 0;
+                A1p.play();
                 break;
             case 200:
-                A2.currentTime = 0;
-                A2.play();
+                A2p.currentTime = 0;
+                A2p.play();
                 break;
             case 300:
-                A3.currentTime = 0;
-                A3.play();
+                A3p.currentTime = 0;
+                A3p.play();
                 break;
             case 400:
-                A4.currentTime = 0;
-                A4.play();
+                A4p.currentTime = 0;
+                A4p.play();
                 break;
             case 500:
-                A5.currentTime = 0;
-                A5.play();
+                A5p.currentTime = 0;
+                A5p.play();
                 break;
             case 600:
-                A6.currentTime = 0;
-                A6.play();
+                A6p.currentTime = 0;
+                A6p.play();
                 break;
             case 700:
-                A7.currentTime = 0;
-                A7.play();
+                A7p.currentTime = 0;
+                A7p.play();
                 break;
             case 800:
-                A8.currentTime = 0;
-                A8.play();
+                A8p.currentTime = 0;
+                A8p.play();
                 break;
             case 900:
-                A9.currentTime = 0;
-                A9.play();
+                A9p.currentTime = 0;
+                A9p.play();
                 break;
-            case 100:
-                A10.currentTime = 0;
-                A10.play();
+            case 1000:
+                A10p.currentTime = 0;
+                A10p.play();
                 break;
             case 1100:
-                A11.currentTime = 0;
-                A11.play();
+                A11p.currentTime = 0;
+                A11p.play();
                 break;
             case 1200:
-                A12plus.currentTime = 0;
-                A12plus.play();
+                A12p.currentTime = 0;
+                A12p.play();
                 break;
     }
 }
@@ -193,7 +193,4 @@ function playSound(tone) {
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-
-    var element = document.h2;
-    element.classList.toggle("h2-dark-mode");
   }
